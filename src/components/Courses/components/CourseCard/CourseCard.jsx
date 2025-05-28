a772fc37dc97fd662357e7993da897623c9d472c
IhorBashtt <bashtannik.igor.v@gmail.com>
2025-05-28 23:10:02 +0300

    CourseCard.jsx

--- src/components/Courses/components/CourseCard/CourseCard.jsx
+++ src/components/Courses/components/CourseCard/CourseCard.jsx
@@ -1,3 +1,10 @@
+import React from "react";
+import { getCourseDuration, formatCreationDate } from "../../../../helpers";
+import styles from "./styles.module.css";
+import { Button } from "../../../../common";
+import deleteIcon from "../../../../assets/deleteButtonIcon.svg";
+import editIcon from "../../../../assets/editButtonIcon.svg";
+
 // Module 1.
 // * figma link: https://www.figma.com/design/m0N0SGLclqUEGR6TUNvyn9/Fundamentals-Courses?node-id=2905-67147&t=OXbHXwMixWTtxRSw-1
 // * render this component inside 'Courses' component
@@ -35,45 +42,51 @@
 //   ** CourseCard should display authors list.
 //   ** CourseCard should display created date in the correct format.
 
-import React from "react";
-
-import { getCourseDuration, formatCreationDate } from "../../../../helpers";
-
-import deleteIcon from "../../../../assets/deleteButtonIcon.svg";
-import editIcon from "../../../../assets/editButtonIcon.svg";
-
-import styles from "./styles.module.css";
-
-export const CourseCard = ({ course, handleShowCourse, authorsList }) => {
-  // write your code here
-
+export const CourseCard = ({
+  course,
+  handleShowCourse,
+  authorsList,
+  handleDeleteCourse,
+  handleUpdateCourse,
+}) => {
   return (
     <div className={styles.cardContainer} data-testid="courseCard">
       <div className={styles.cardText}>
-        <h2>Title</h2>
-        <p>Description</p>
+        <h2>{course.title}</h2>
+        <p>{course.description}</p>
       </div>
       <div className={styles.cardDetails}>
         <p>
           <b>Authors: </b>
-          authors list
+          {course.authors
+            .map((id) => {
+              const author = authorsList.find((a) => a.id === id);
+              return author ? author.name : "Unknown Author";
+            })
+            .join(", ")}
         </p>
         <p>
-          <b>Duration:</b>
-          <span>duration</span>
+          <b>Duration: </b> <span>{getCourseDuration(course.duration)}</span>
         </p>
         <p>
           <b>Created: </b>
-          <span>date</span>
+          <span>{formatCreationDate(course.creationDate)}</span>
         </p>
         <div className={styles.buttonsContainer}>
-          {/* 
-				reuse Button component for 'Show course' button 
-				reuse Button	component with deleteButtonIcon from 'src/assets' for 'Delete' button
-						with data-testid="deleteCourse" 
-				reuse Button component wrapped with Link from react-router with editButtonIcon from 'src/assets' for 'Update' button with
-						data-testid="updateCourse" 
-			*/}
+          <Button
+            buttonText="SHOW COURSE"
+            handleClick={() => handleShowCourse(course.id)}
+          />
+          <Button
+            buttonText={<img src={deleteIcon} alt="Delete course" />}
+            handleClick={() => handleDeleteCourse(course.id)}
+            data-testid={`deleteCourse-${course.id}`}
+          />
+          <Button
+            buttonText={<img src={editIcon} alt="Update course" />}
+            handleClick={() => handleUpdateCourse(course.id)}
+            data-testid={`updateCourse-${course.id}`}
+          />
         </div>
       </div>
     </div>
