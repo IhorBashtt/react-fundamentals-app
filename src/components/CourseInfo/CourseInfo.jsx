625350c544b1921c751f84ce444ab9a04c22a5d9
IhorBashtt <bashtannik.igor.v@gmail.com>
2025-05-29 12:06:04 +0300

    CourseInfo.jsx

--- src/components/CourseInfo/CourseInfo.jsx
+++ src/components/CourseInfo/CourseInfo.jsx
@@ -1,94 +1,75 @@
 import React from "react";
+
+import { formatCreationDate, getCourseDuration } from "../../helpers";
 import styles from "./styles.module.css";
 import { Button } from "../../common";
-import { CourseCard } from "./components/CourseCard";
+// This component shows information about the current chosen course.
 
-// Module 1:
-// * render list of components using 'CourseCard' component for each course
-// * render 'ADD NEW COURSE' button (reuse Button component)
-// ** TASK DESCRIPTION ** - https://react-fundamentals-tasks.vercel.app/docs/module-1/home-task/components#courses-component
-// * render EmptyCourseList component when no courses
-// ** TASK DESCRIPTION ** - https://react-fundamentals-tasks.vercel.app/docs/module-1/home-task/components#emptycourselist-component
-// * DO NOT map authors to the course inside Courses.jsx component (DO it inside CourseCard)
+// Module 1.
+// * Use template to show course's information:
+// ** ID of course;
+// ** Title;
+// ** Description;
+// ** Duration;
+// ** List of authors;
+// ** Creation date;
+// * use <Button /> component to replace CourseInfo component with Courses component
+// ** TASK DESCRIPTION ** - https://react-fundamentals-tasks.vercel.app/docs/module-1/home-task/components#course-info
 
-// Module 2:
-// * render this component by route '/courses'
-// * navigate to this component if 'localStorage' contains user's token
-// * navigate to the route courses/add by clicking 'Add New Course' button, use 'Link' component from 'react-router-dom'
-// ** TASK DESCRIPTION ** - https://react-fundamentals-tasks.vercel.app/docs/module-2/home-task/components#courses
+// Module 2.
+// * render component by route '/courses/:courseId'
+// * use 'useParam' hook to get course id, remove prop 'showCourseId'
+// * remove 'onBack' prop
+// * use '<Link />' instead <Button /> component for 'BACK' button
+// ** TASK DESCRIPTION ** - https://react-fundamentals-tasks.vercel.app/docs/module-2/home-task/components#course-info
 
-// Module 3:
-// * stop using mocked courses and authors data
-// * delete props 'coursesList' and 'authorsList'
-// * use useSelector to get courses and authors from the store
-// ** TASK DESCRIPTION ** - https://react-fundamentals-tasks.vercel.app/docs/module-3/home-task/components#courses-component
+// Module 3.
+// * remove props 'coursesList', 'authorsList'
+// * use selectors from store/selectors.js to get coursesList, authorsList from store
 
-// Module 4:
-// navigate to '/courses/add' route by clicking 'ADD NEW COURSE' button in the 'EmptyCourseList'.
-// show message 'You don't have permissions to create a course. Please log in as ADMIN' by clicking ADD NEW COURSE button in the 'EmptyCourseList'.
-// ** TASK DESCRIPTION ** - https://react-fundamentals-tasks.vercel.app/docs/module-4/home-task/components#emptycourselist-component
+// props description
+// * 'coursesList' - list of all courses. You need it to get chosen course from the list
+// * 'authorsList' - list of all authors. You need it to get authors' names for chosen course
+// * 'showCourseId' - id of chosen course. Use it to find needed course on the 'coursesList'.
+export const CourseInfo = ({
+  coursesList,
+  authorsList,
+  onBack,
+  showCourseId,
+}) => {
+  const course = coursesList.find((course) => course.id === showCourseId);
 
-// Module 5:
-// * proposed cases for unit tests:
-//   ** Courses should display amount of CourseCard equal length of courses array.
-//   ** CourseForm should be shown after a click on the "Add new course" button.
-
-const EmptyCourseList = ({ onAddClick }) => {
   return (
-    <div className={styles.empty} data-testid="emptyContainer">
-      <h2>Your List Is Empty</h2>
-      <p>Please use "add new course" button to add your first course</p>
-      <div className={styles.buttonContainer}>
-        <Button
-          buttonText="ADD NEW COURSE"
-          data-testid="addCourse"
-          handleClick={onAddClick}
-        />
+    <div className={styles.container} data-testid="courseInfo">
+      <h1>{course.title}</h1>
+      <div className={styles.courseInfo}>
+        <p className={styles.description}>{course.description}</p>
+        <div>
+          <p>
+            <b>ID: </b>
+            {course.id}
+          </p>
+          <p>
+            <b>Duration: </b>
+            {getCourseDuration(course.duration)}
+          </p>
+          <p>
+            <b>Created: </b>
+            {formatCreationDate(course.creationDate)}
+          </p>
+          <div>
+            <b>Authors</b>
+            <ul className={styles.authorsList}>
+              {course.authors.map((id) => (
+                <li key={id}>
+                  {authorsList.find((author) => author.id === id)?.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
+      <Button buttonText="BACK" handleClick={onBack} />
     </div>
   );
 };
-
-// Основной компонент Courses
-export const Courses = ({
-  coursesList,
-  authorsList,
-  handleShowCourse,
-  handleDeleteCourse,
-  handleUpdateCourse,
-  onAddClick,
-  searchQuery,
-}) => {
-  // Фильтруем курсы по названию (без учета регистра)
-  const filteredCourses = coursesList.filter((course) =>
-    course.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  if (filteredCourses.length === 0) {
-    return <EmptyCourseList onAddClick={onAddClick} />;
-  }
-
-  return (
-    <>
-      <div className={styles.panel} key="add-new-course">
-        <Button
-          buttonText="ADD NEW COURSE"
-          data-testid="addCourse"
-          handleClick={onAddClick}
-        />
-      </div>
-      {filteredCourses.map((course) => (
-        <CourseCard
-          key={course.id}
-          course={course}
-          authorsList={authorsList}
-          handleShowCourse={handleShowCourse}
-          handleDeleteCourse={handleDeleteCourse}
-          handleUpdateCourse={handleUpdateCourse}
-        />
-      ))}
-    </>
-  );
-};
-
-export { EmptyCourseList };
